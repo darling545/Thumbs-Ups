5e19a246fc64114e5966e6817f8e818b67d0b290
zhaojd <764534336@qq.com>
2025-04-17 23:38:50 +0800

    first test  commit

---
+++ src/main/java/com/shen/thumbsups/ThumbsUpsApplication.java
@@ -0,0 +1,13 @@
+package com.shen.thumbsups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThumbsUpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThumbsUpsApplication.class, args);
+    }
+
+}
